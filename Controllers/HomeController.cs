37e16aa37898975d5b0c22a089c1115bfd0dd706
Yogesh-Rajpure <yrajpure1@gmail.com>
2023-07-16 15:29:15 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace SpeedCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalculateSpeed(double speed)
+        {
+            // Convert m/s to km/h
+            double speedKmPerHour = speed * 3.6;
+
+            // Store the result in ViewBag
+            ViewBag.SpeedKmPerHour = speedKmPerHour;
+
+            return View("Index");
+        }
+    }
+}
